ac6b6a1792096fb65bd265040689e579ae340ee2
Anderson Barrera <andersonbar10@gmail.com>
2024-01-04 22:50:51 -0500

    Agrego Power BI

--- script.js
+++ script.js
@@ -29,12 +29,13 @@ function efectoHabilidades(){
         habilidades[2].classList.add("phyton");
         habilidades[3].classList.add("sql");
         habilidades[4].classList.add("excel");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("atencionAlCliente");
-        habilidades[7].classList.add("trabajoEnEquipo");
-        habilidades[8].classList.add("creatividad");
-        habilidades[9].classList.add("dedicacion");
-        habilidades[10].classList.add("agilidadDeResultados");
+        habilidades[5].classList.add("powerbi");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("atencionAlCliente");
+        habilidades[8].classList.add("trabajoEnEquipo");
+        habilidades[9].classList.add("creatividad");
+        habilidades[10].classList.add("dedicacion");
+        habilidades[11].classList.add("agilidadDeResultados");
     }
 }
 
